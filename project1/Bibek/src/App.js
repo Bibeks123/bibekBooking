c919f6131e04cd1d9e0d3f14a73a62d89d2729c5
bibek subedi <subedi.bibek164@gmail.com>
2022-07-12 00:27:37 +0545

    initial commit

---
+++ project1/Bibek/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+
+function App (){
+  return(
+    <View>
+      <Text>
+        hi my name is bibeks
+      </Text>
+    </View>
+  )
+}
+export default App
